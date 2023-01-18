e036118c64aaa4707ca83a7275b2efec0a6f138b
zimbatm <zimbatm@zimbatm.com>
2023-01-18 18:36:56 +0100

    flake: add nix development environment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # Languages
+            pkgs.crystal
+            pkgs.nodejs
+            pkgs.shards
+            # Build deps
+            pkgs.gmp
+            pkgs.openssl
+            pkgs.sqlite
+            # Deploy
+            pkgs.flyctl
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
